6843ac1d5de4e6afd974d5de0536de9b12ea8eff
Sakthivel Nachimuthu <17950332+dcnsakthi@users.noreply.github.com>
2024-02-18 22:26:48 +0800

    Create databricks_notebooks_connecting_with_microsoft_fabric_onelake.py

---
+++ fabric/databricks/databricks_notebooks_connecting_with_microsoft_fabric_onelake.py
@@ -0,0 +1,29 @@
+# Databricks Notebooks Connecting with Microsoft Fabric OneLake
+
+# Step #1: Create a Service Principal in Microsoft Entra ID (Azure Active Directory) - To get started, create a Service Principal in Microsoft Entra ID (Azure Active Directory) with the required credentials, ensuring that Microsoft Fabric OneLake is present in the same tenant.
+# Step #2: Grant Permission to the Service Principal - Next, grant the appropriate permission to the Service Principal within the Microsoft Fabric workspace, allowing it to read data from the OneLake storage effectively.
+# Step #3: Create Notebooks in Databricks - Utilize Databricks notebooks to read data from the Microsoft Fabric OneLake Lakehouse endpoint using Spark compute. These notebooks should include the Service Principal credentials (Tenant ID, Client ID, Client Secret) to establish secure access to the OneLake data.
+
+# Import the necessary libraries
+from pyspark.sql import SparkSession
+
+# Configure the Service Principal credentials
+spark.conf.set("fs.azure.account.auth.type.onelake.dfs.fabric.microsoft.com", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type.onelake.dfs.fabric.microsoft.com", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id.onelake.dfs.fabric.microsoft.com", {*****clientId*****} )
+spark.conf.set("fs.azure.account.oauth2.client.secret.onelake.dfs.fabric.microsoft.com", {*****clientSecret******} )
+spark.conf.set("fs.azure.account.oauth2.client.endpoint.onelake.dfs.fabric.microsoft.com", "https://login.microsoftonline.com/"+{*****tenantId******}+"/oauth2/token")
+
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("Read Data from Microsoft Fabric") \
+    .getOrCreate()
+
+# Read the data from Microsoft Fabric
+df = spark.read.format("delta") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load("abfss://{***WorkspaceName***}@onelake.dfs.fabric.microsoft.com/{**LakehoueName**}.Lakehouse/Tables/{****TableName****}")
+
+# Display the DataFrame
+display(df)
